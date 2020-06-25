345c9357f517fd850ebb63d86c0bc6979cbe22bf
Cvqtko <cvqtkokirilov@gmail.com>
2020-06-25 14:53:41 +0300

    Solutions to exercises JS syntax, functions and statements

---
+++ 02_JS Syntax, Functions and Statements Exercise/exercise_01.js
@@ -0,0 +1,14 @@
+function solve(fruit, weightInGrams, price) {
+
+	const weightInKilos = weightInGrams / 1000;
+
+	const totalPrice = weightInKilos * price;
+
+	
+	console.log('I need $' + totalPrice.toFixed(2) + ' to buy ' + weightInKilos.toFixed(2) + ' kilograms ' + fruit
+			+ '.')
+
+}
+
+solve('orange', 2500, 1.80);
+solve('apple', 1563, 2.35);
