291ae602574d41b9710e48fc929f6dd64db70d65
Cvqtko <cvqtkokirilov@gmail.com>
2020-06-25 15:24:53 +0300

    Exercises JS

--- 02_JS Syntax, Functions and Statements Exercise/exercise_02.js
+++ 02_JS Syntax, Functions and Statements Exercise/exercise_02.js
@@ -1,5 +1,13 @@
-function solve() {
+function solve(a, b) {
+	const small = Math.min(a, b);
+	let gcd = 1;
+	for (let i = 1; i <= small; i++) {
+		if (a % i == 0 && b % i == 0) {
+			gcd = i;
+		}
+	}
 
+	console.log(gcd);
 }
 
-solve();
+solve(15, 5);
