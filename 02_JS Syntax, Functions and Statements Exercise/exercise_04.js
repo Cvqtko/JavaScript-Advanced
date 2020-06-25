38f1fb4f2c2e643a99ab24843d584506db1fcaf9
Cvqtko <cvqtkokirilov@gmail.com>
2020-06-25 15:55:48 +0300

    JS exercises

--- 02_JS Syntax, Functions and Statements Exercise/exercise_04.js
+++ 02_JS Syntax, Functions and Statements Exercise/exercise_04.js
@@ -1,5 +1,25 @@
-function solve() {
+function solve(steps, footprint, speed) {
+	const distInKM = steps * footprint;
+	let timeInSeconds = Math.round(distInKM / speed * 3.6);
 
+	timeInSeconds += Math.floor(distInKM / 500) * 60;
+
+	const seconds = timeInSeconds % 60;
+	timeInSeconds -= seconds;
+	const minutes = (timeInSeconds / 60) % 60;
+	timeInSeconds -= minutes * 60;
+	const hours = timeInSeconds / 3600;
+
+	console.log(`${pad(hours)}:${pad(minutes)}:${pad(seconds)}`);
+
+	function pad(num) {
+		if (num < 10) {
+			return '0' + num;
+		}
+		return '' + num;
+	}
 }
 
-solve();
+solve(4000, 0.60, 5);
+
+solve(2564, 0.70, 5.5);
