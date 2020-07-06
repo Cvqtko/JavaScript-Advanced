87b83cb8cffe20e695f5a0bdd218ceb9578d796f
Cvqtko <cvqtkokirilov@gmail.com>
2020-07-06 20:38:51 +0300

    Exercises on Objects and Classes

--- 05 Objects and Classes/exercise_04.js
+++ 05 Objects and Classes/exercise_04.js
@@ -1,5 +1,17 @@
-function solve() {
+function JsonToHtmlTable(json) {
+  let arr = JSON.parse(json);
+  let outputArr = ["<table>"];
+  outputArr.push(makeKeyRow(arr));
+  arr.forEach((obj) => outputArr.push(makeValueRow(obj)));
+  outputArr.push("</table>"); 
+  function makeKeyRow(arr) { //  ToDo 
+	};
 
+  function makeValueRow(obj) { //  ToDo
+	};
+
+  function escapeHtml(value) { //  ToDo
+	 };
+
+  console.log(outputArr.join('\n'));
 }
-
-solve();
