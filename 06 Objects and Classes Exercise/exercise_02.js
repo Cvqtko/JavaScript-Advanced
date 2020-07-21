245312f1b95568cb7f4ca0227327ae0f496cc285
Cvqtko <cvqtkokirilov@gmail.com>
2020-07-21 19:42:12 +0300

    Objects and Classes exercise

--- 06 Objects and Classes Exercise/exercise_02.js
+++ 06 Objects and Classes Exercise/exercise_02.js
@@ -1,12 +1,12 @@
 function jsonsTable (inputData) {
     let html = '<table>\n'
     for(let row of inputData){
-        let parsed = JSON.parse(row)
-        html += '  <tr>\n'
-        html += '     <td>' + parsed.name + '</td>\n'
-        html += '     <td>' + parsed.position + '</td>\n'
-        html += '     <td>' + parsed.salary + '</td>\n'
-        html += '  <tr>\n'
+        let person = JSON.parse(row)
+        html += '   <tr>\n'
+        html += '\t<td>' + person.name + '</td>\n'
+        html += '\t<td>' + person.position + '</td>\n'
+        html += '\t<td>' + person.salary + '</td>\n'
+        html += '   <tr>\n'
     }
     html += '</table>'
     return html
