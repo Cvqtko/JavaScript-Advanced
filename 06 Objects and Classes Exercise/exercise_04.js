7e6405e386a1c6eb5515ae467b21b80dd77f84a6
Cvqtko <cvqtkokirilov@gmail.com>
2020-07-28 15:03:54 +0300

    Objects and Classes exercises

--- 06 Objects and Classes Exercise/exercise_04.js
+++ 06 Objects and Classes Exercise/exercise_04.js
@@ -1,5 +1,38 @@
-function solve() {
-
+function solve(inputArr) {
+	let sortedArr = inputArr.sort();
+	
+	let firstLetter = sortedArr[0][0];
+	
+	console.log(firstLetter);
+	
+	for(let string of sortedArr){
+		if(string[0]!==firstLetter){
+			firstLetter = string[0];
+			console.log(firstLetter);
+		}
+		let[key, value] = string.split(" : ");
+		console.log(` ${key}: ${value}`);
+	}
 }
 
-solve();
+solve(
+		['Appricot : 20.4',
+			'Fridge : 1500',
+			'TV : 1499',
+			'Deodorant : 10',
+			'Boiler : 300',
+			'Apple : 1.25',
+			'Anti-Bug Spray : 15',
+			'T-Shirt : 10']
+);
+console.log('--------------------------');
+solve(
+		['Banana : 2',
+			'Rubic\'s Cube : 5',
+			'Raspberry P : 4999',
+			'Rolex : 100000',
+			'Rollon : 10',
+			'Rali Car : 2000000',
+			'Pesho : 0.000001',
+			'Barrel : 10']
+);
---
+++ 06 Objects and Classes Exercise/test.js
@@ -0,0 +1,13 @@
+function solve() {
+	var list = {
+		"aou" : 100,
+		"me" : 75,
+		"foo" : 116,
+		"bar" : 15,
+	};
+	keysSorted = Object.keys(list).sort(function(a, b) {
+		return list[a] - list[b]
+	})
+	console.log(keysSorted);
+}
+solve();
