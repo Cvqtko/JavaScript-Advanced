932d8630d43fae4ee9e251e81fad5d2385c2ab76
Cvqtko <cvqtkokirilov@gmail.com>
2020-08-18 18:12:54 +0300

    Arrays exercises

---
+++ 03 Arrays/mmutable-array-test.js
@@ -0,0 +1,11 @@
+let someArr = [1,2,3,4];
+
+function addElement(arr,el){
+	arr.push(el);
+}
+
+addElement(someArr, 5);
+console.log(someArr);
+
+/* Mutable is a type of variable that can be changed. 
+In JavaScript, only objects and arrays are mutable, not primitive values. */
