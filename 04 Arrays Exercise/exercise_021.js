41452dae2af3e66b5f114256fbd58d441e99bd26
Cvqtko <cvqtkokirilov@gmail.com>
2020-08-23 17:37:11 +0300

    Arrays exercises

--- 04 Arrays Exercise/exercise_021.js
+++ 04 Arrays Exercise/exercise_021.js
@@ -1,7 +1,7 @@
 function solve(arr) {
 	const step = Number(arr.pop());
 
-	return arr.filter((x,i) => i %step=== 0 ).join('\n');
+	return arr.filter((x,i) => i % step === 0 ).join('\n');
 }
 
 console.log(solve([ '1', '2', '3', '4', '5', '6' ]));
--- 04 Arrays Exercise/exercise_07.js
+++ 04 Arrays Exercise/exercise_07.js
@@ -22,3 +22,4 @@ function magicMatrix(matrix) {
 console.log(magicMatrix([ [ 4, 5, 6 ], [ 6, 5, 4 ], [ 5, 5, 5 ] ]));
 console.log(magicMatrix([ [ 11, 32, 45 ], [ 21, 0, 1 ], [ 21, 1, 1 ] ]));
 console.log(magicMatrix([ [ 1, 0, 0 ], [ 0, 0, 1 ], [ 0, 1, 0 ] ]));
+console.log(magicMatrix([ [ 4 ]]));
