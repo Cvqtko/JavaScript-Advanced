04bed25e14e69f58f87b62ffb30cb8c9c6db9ef4
Cvqtko <cvqtkokirilov@gmail.com>
2020-08-27 08:33:50 +0300

    Exercises

--- 05 Objects and Classes/exercise_01.js
+++ 05 Objects and Classes/exercise_01.js
@@ -1,4 +1,15 @@
-function solve() {
-	
+class Person {
+    constructor(fName, lName, age, email) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.age = age;
+        this.email = email;
+    }
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+    }
 }
-solve();
+
+let person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+console.log(person.toString());
+
