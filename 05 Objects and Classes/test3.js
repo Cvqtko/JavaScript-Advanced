08fd635957d8cd6fff4fe467bdfa417a93379821
Cvqtko <cvqtkokirilov@gmail.com>
2020-11-25 14:03:13 +0200

    Exercises

--- 05 Objects and Classes/test3.js
+++ 05 Objects and Classes/test3.js
@@ -1,4 +1,6 @@
-let arr = [ 1, 2, 3, 4, 5 ];
-arr[10] = 10;
-console.log(arr.length);
-console.log(arr);
+let x = {name : "John"};
+let y = x;
+y.name = "Pesho";
+
+console.log(x.name);
+console.log(y.name);
