5e62cb441a14f2b0c609e8add48c1df7b81b795b
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-04 15:51:50 +0200

    Exercises

--- 06 Objects and Classes Exercise/more_exercise_7.js
+++ 06 Objects and Classes Exercise/more_exercise_7.js
@@ -1,5 +1,22 @@
-function solve() {
-
+function solve(input) {
+   let arrays = new Map()
+    for (let i = 0; i < input.length; i++) {
+        let currentArray = JSON.parse(input[i]).map(Number).sort((a,b) => b - a)
+        let toAdd = currentArray.join(', ')
+        if(!arrays.has(toAdd)){
+            arrays.set(toAdd, currentArray.length)
+        }
+    }
+    let result = Array.from(arrays.keys()).sort((a,b) => arrays.get(a) - arrays.get(b))
+    result.forEach(a => console.log(`[${a}]`))
 }
 
-solve();
+solve(["[-3, -2, -1, 0, 1, 2, 3, 4]",
+"[10, 1, -17, 0, 2, 13]",
+"[4, -3, 3, -2, 2, -1, 1, 0]"]
+);
+
+solve(["[7.14, 7.180, 7.339, 80.099]",
+"[7.339, 80.0990, 7.140000, 7.18]",
+"[7.339, 7.180, 7.14, 80.099]"]
+);
--- 06 Objects and Classes Exercise/more_exercise_8.js
+++ 06 Objects and Classes Exercise/more_exercise_8.js
@@ -1,5 +1,39 @@
-function solve() {
+let result = (function(){
+    let validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    let validSuits = ['♠', '♥', '♦', '♣'];
+    let Suits = {
+        SPADES: '♠',
+	HEARTS: '♥',
+	DIAMONDS: '♦',
+	CLUBS: '♣',
+    };
 
-}
+    class Card {
+	constructor(face, suit) {
+	    if (!validFaces.includes(face)) {
+		throw new Error('Invalid Card Face!');
+	    }
+	    if (!validSuits.includes(suit)) {
+		throw new Error('Invalid Card Suit!');
+	    }
+  	    this.face = face;
+	    this.suit = suit;
+	}
+    }
 
-solve();
+    return {
+	Suits: Suits,
+	Card: Card
+    }
+}());
+
+let Card = result.Card;
+let Suits = result.Suits;
+
+let card = new Card("Q", Suits.CLUBS);
+card.face = "A";
+card.suit = Suits.DIAMONDS;
+console.log(card);
+
+let card2 = new Card("1",Suits.DIAMONDS);
+console.log(card2);
