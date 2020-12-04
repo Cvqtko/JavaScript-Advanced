7e7dc4940ea3e825f94c070bd002ce826d04414e
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-04 15:29:24 +0200

    Exercises

--- 06 Objects and Classes Exercise/more_exercise_5.js
+++ 06 Objects and Classes Exercise/more_exercise_5.js
@@ -1,5 +1,37 @@
-function solve() {
-
+class Rat {
+    constructor(name) {
+        this.name = name
+        this.Rats = []
+    }
+    unite(obj) {
+        if (obj.constructor.name == 'Rat') {
+            this.Rats.push(obj)
+        }
+    }
+    getRats() {
+        return this.Rats
+    }
+    toString(){
+        let names = this.name
+        for (let rat of this.Rats){
+           names+=`\n##${rat.name}`
+        }
+        return names
+    }
 }
 
-solve();
+let firstRat = new Rat("Peter");
+console.log(firstRat.toString()); // Peter
+ 
+console.log(firstRat.getRats()); // []
+
+firstRat.unite(new Rat("George"));
+firstRat.unite(new Rat("Alex"));
+console.log(firstRat.getRats());
+// [ Rat { name: 'George', unitedRats: [] },
+//  Rat { name: 'Alex', unitedRats: [] } ]
+
+console.log(firstRat.toString());
+// Peter
+// ##George
+// ##Alex
--- 06 Objects and Classes Exercise/more_exercise_6.js
+++ 06 Objects and Classes Exercise/more_exercise_6.js
@@ -1,5 +1,29 @@
-function solve() {
-
+function solve(arr) {
+    let set = new Set();
+    for (let input of arr) {
+        set.add(input);
+    }
+    let userNames = [...set.keys()].sort((a, b) => sortFunc(a, b));
+    for (let users of userNames) {
+        console.log(users);
+    }
+}
+ 
+function sortFunc(a, b) {
+    if (a.length != b.length) {
+        return a.length - b.length;
+    }
+    else {
+        return a.localeCompare(b);
+    }
 }
 
-solve();
+solve(['Ashton',
+'Kutcher',
+'Ariel',
+'Lilly',
+'Keyden',
+'Aizen',
+'Billy',
+'Braston']
+);
