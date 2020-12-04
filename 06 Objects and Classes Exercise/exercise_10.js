162ae49a2e20488bcbc22e2abffcadc356c6bed0
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-04 14:57:57 +0200

    Exercises

--- 06 Objects and Classes Exercise/exercise_10.js
+++ 06 Objects and Classes Exercise/exercise_10.js
@@ -1,5 +1,30 @@
-function solve() {
-
+class Stringer{
+	 constructor(initialString, length) {
+        this.initialString = initialString;
+        this.length = length;
+    }
+	increase(length){
+		 this.length = this.length + length;
+	}
+	decrease(length){
+		this.length = this.length - length > 0? this.length - length:0;
+	}
+	toString(){
+		if(this.length >= this.initialString.length){
+			return this.initialString.substring(0, this.length);
+		}else{
+			return this.initialString.substring(0, this.length) + `...`;
+		}
+	}
 }
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
 
-solve();
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4); 
+console.log(test.toString()); // Test
