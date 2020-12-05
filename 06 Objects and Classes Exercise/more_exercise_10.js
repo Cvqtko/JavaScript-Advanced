d071f212558a4c9f73a079ded89bf5572fd51717
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-05 12:11:11 +0200

    Exercises

--- 06 Objects and Classes Exercise/more_exercise_10.js
+++ 06 Objects and Classes Exercise/more_exercise_10.js
@@ -1,5 +1,25 @@
-function solve() {
-
+class Extensible{
+    static counter = 0;
+    
+    constructor(){
+        this.id = ++Extensible.counter;
+    }
+    
+   extend(template){
+        for(let parentProp of Object.keys(template)){
+            if(typeof(template[parentProp]) == "function"){
+                Object.getPrototypeOf(this)[parentProp] = template[parentProp];
+            } else {
+                this[parentProp] = template[parentProp];
+            }
+        }
+    }
+   
 }
-
-solve();
+ let obj1 = new Extensible();
+ let obj2 = new Extensible();
+ let obj3 = new Extensible();
+ console.log(obj1.id);
+ console.log(obj2.id);
+ console.log(obj3.id);
+ 
