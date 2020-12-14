8c99d1d3e2df1811347c84cf2eb5bed12a120225
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-14 19:33:27 +0200

    Exercises

--- 08 Document Object Model Exercise/06. Numpad-Calculator/solution.js
+++ 08 Document Object Model Exercise/06. Numpad-Calculator/solution.js
@@ -1,4 +1,53 @@
 function solve() {
 
-    //TODO...
+	let expression = document.getElementById('expressionOutput');
+	let result = document.getElementById('resultOutput');
+
+	document.querySelector('.keys').addEventListener('click', symbolClicked);
+
+	document.querySelector('.clear').addEventListener('click', clear);
+
+
+	function clear() {
+		expression.textContent = '';
+		result.textContent = '';
+	}
+
+	function symbolClicked(event) {
+		let buttonPressed = event.target.value;
+
+		switch (buttonPressed) {
+			case "/":
+			case "*":
+			case "+":
+			case "-":
+				expression.textContent += ` ${buttonPressed} `;
+				break;
+			case "=":
+				let [leftOperand, operator, rightOperand] = expression.textContent.split(' ');
+				if (!operator || !rightOperand) {
+					result.textContent = 'NaN';
+				} else {
+					let operationResult = '';
+					switch (operator) {
+						case "/": operationResult = Number(leftOperand)/ Number(rightOperand);
+						break;
+						case "*":  operationResult = Number(leftOperand)* Number(rightOperand);
+						 break;
+						case "+":  operationResult = Number(leftOperand)+ Number(rightOperand);
+						 break;
+						case "-":  operationResult = Number(leftOperand)- Number(rightOperand);
+						break;
+					}
+					result.textContent = operationResult;
+				}
+				break;
+			default:
+				expression.textContent += buttonPressed;
+		}
+
+	}
+
+
 }
+
