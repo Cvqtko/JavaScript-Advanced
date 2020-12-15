74eb9f050b22d7b75e3e68c12c86f9f48488ecb2
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-15 19:37:59 +0200

    Exercises

--- 10 DOM Manipulations Exercise/02. Notification/scripts.js
+++ 10 DOM Manipulations Exercise/02. Notification/scripts.js
@@ -1,3 +1,12 @@
 function notify(message) {
-    console.log('TODO:...');
+	let divNotification = document.getElementById('notification');
+
+	divNotification.textContent = message;
+
+	divNotification.style.display = 'block';
+	
+	// will wait for the 
+	setTimeout(() => {
+		divNotification.style.display = 'none';
+	}, 2000);
 }
