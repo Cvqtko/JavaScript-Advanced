0cefa3547ae5e0ada9e59faaeac6c69b47f46767
Cvqtko <cvqtkokirilov@gmail.com>
2020-12-15 17:49:45 +0200

    Exercise

--- 09 DOM Manipulations/02. Add-Delete/app.js
+++ 09 DOM Manipulations/02. Add-Delete/app.js
@@ -1,3 +1,31 @@
 function solve() {
-  // TODO:
+	let itemsListElement = document.getElementById('items');
+
+	let inputElement = document.getElementById('newText');
+
+	let liElement = document.createElement('li');
+
+	if (inputElement.value != '') {
+		
+		// first way
+		// liElement.innerHTML = `${inputElement.value} <span class = "delete">[delete]<span>`;
+		
+		// second way
+		 liElement.innerHTML = inputElement.value;
+		let deleteElement = document.createElement('span');
+		deleteElement.innerText = '[delete]';
+		deleteElement.style.cursor = 'pointer';
+		deleteElement.style.color = 'red';
+		
+		liElement.appendChild(deleteElement);
+		
+		deleteElement.addEventListener('click', function(e){
+			let parentElement = e.target.parentElement;
+			parentElement.remove();
+		});
+			
+		itemsListElement.appendChild(liElement);
+
+		inputElement.value = '';
+	}
 }
